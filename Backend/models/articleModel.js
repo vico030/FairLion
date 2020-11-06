c617741e24da58a1256f5212917358b8d1441b98
slimeconomy <kernezitrone@gmail.com>
2020-11-06 11:18:08 +0100

    adding articleModel

---
+++ Backend/models/articleModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { ObjectId } = require("mongoose");
+
+const articleSchema = mongoose.Schema({
+    title: {
+        required: true,
+        type: String
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    duration: {
+        required: true,
+        type: String
+    },
+    images: {
+        required: true,
+        type: [String]
+    },
+    status: {
+        required: true,
+        type: String
+    },
+    owner: {
+        required: true,
+        type: ObjectId
+    },
+    borrower: {
+        required: true,
+        type: ObjectId
+    }
+})
+
+module.exports = mongoose.model(articleSchema);
