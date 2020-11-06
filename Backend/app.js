2b190a889540150029233a398be0b0b3b9affbf8
marvellousmarvolo <la0gueule@gmail.com>
2020-11-06 16:01:24 +0100

    created basic project structure

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var allRoutes = require('./routes/allRoutes');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// Routes
+app.use('/', allRoutes.indexRoute);
+app.use('/users', allRoutes.usersRoute);
+app.use('/articles', allRoutes.articlesRoute);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ Backend/routes/allRoutes.js
@@ -0,0 +1,9 @@
+var indexRoute = require('./index');
+var usersRoute = require('./users');
+var articlesRoute = require('./articles');
+
+module.exports = {
+    indexRoute,
+    usersRoute,
+    articlesRoute
+};
