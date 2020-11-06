58e4cbe6887d0addd0fed52cb6dab9ede031ed1d
John <einickejohn@gmail.com>
2020-11-06 11:31:08 +0100

    Fixing file names

--- Backend/models/categoryModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const category = mongoose.model("Category",
-    {
-        name: { type: String, unique: true, required: true, dropDups: true }
-    });
-
-module.exports = category;
