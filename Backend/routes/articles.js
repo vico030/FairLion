65d573a65c713aeafabec20d0eed988bf53ec5cc
John <einickejohn@gmail.com>
2020-11-16 21:59:35 +0100

    Article Route implementiert

--- Backend/routes/articles.js
+++ Backend/routes/articles.js
@@ -4,7 +4,9 @@ const {
 } = require("../helpers/error");
 const router = express.Router();
 const articleService = require('../services/articleService');
+const auth = require("../services/authService");
 
+//get all articles
 router.get("/", async (req, res, next) => {
     try {
         const response = await articleService.getAllArticles();
@@ -21,6 +23,7 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+//update all articles
 router.put("/", async (req, res, next) => {
     try {
         const response = await articleService.updateAllArticles(req.body);
@@ -37,6 +40,7 @@ router.put("/", async (req, res, next) => {
     }
 });
 
+//delete all articles
 router.delete("/", async (req, res, next) => {
     try {
         const response = await articleService.deleteAllArticles();
@@ -52,5 +56,74 @@ router.delete("/", async (req, res, next) => {
         })
     }
 });
+
+//get a single article by id
+router.get("/:articleId", async (req, res, next) => {
+    try {
+        const response = await articleService.getArticleById(req.params.articleId);
+        res.status(response.status).json({
+            'data': response.data,
+            'message': response.message
+        });
+    }
+    catch ({ error, status, message }) {
+        res.status(status).json({
+            'error': error,
+            'message': message
+        })
+    }
+});
+
+//update a single article
+router.put("/:articleId", async (req, res, next) => {
+    try {
+        const response = await articleService.updateArticleById(req.body, req.params.articleId);
+        res.status(response.status).json({
+            'data': response.data,
+            'message': response.message
+        });
+    }
+    catch ({ error, status, message }) {
+        res.status(status).json({
+            'error': error,
+            'message': message
+        })
+    }
+});
+
+//delete a single article
+router.delete("/:articleId", async (req, res, next) => {
+    try {
+        const response = await articleService.deleteArticleById(req.params.articleId);
+        res.status(response.status).json({
+            'data': response.data,
+            'message': response.message
+        });
+    }
+    catch ({ error, status, message }) {
+        res.status(status).json({
+            'error': error,
+            'message': message
+        })
+    }
+});
+
+//get articles from friends by articlename
+router.get("/query/:articlename", auth.isAuthenticated, async (req, res, next) => {
+    try {
+        const response = await articleService.getArticlesFromFriendsByName(req.userId);
+        res.status(response.status).json({
+            'data': response.data,
+            'message': response.message
+        });
+    }
+    catch ({ error, status, message }) {
+        res.status(status).json({
+            'error': error,
+            'message': message
+        })
+    }
+});
+
 
 module.exports = router;
--- Backend/services/articleService.js
+++ Backend/services/articleService.js
@@ -1,8 +1,55 @@
 const Article = require('../models/ArticleModel');
+const User = require("../models/UserModel");
 
 const getAllArticles = function () {
     return new Promise((resolve, reject) => {
         Article.find({})
+            .then((articles) => {
+                return resolve({
+                    data: articles,
+                    message: 'Einträge wurden gefunden.',
+                    status: 200
+                })
+            })
+            .catch((err) => {
+                return reject({
+                    error: err,
+                    status: 500,
+                    message: 'Einträge konnten nicht gefunden werden.'
+                })
+            });
+    });
+}
+
+const getArticlesFromFriendsByName = function (userId) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const articles = {};
+            const user = await User.findById(userId);
+            for (let i = 0; i < user.friends.length; i++) {
+                const { username } = await User.findById(user.friends[i]);
+                const articlesFromFriend = await Article.find({ "owner": user.friends[i] });
+                articles[username] = articlesFromFriend;
+            }
+            return resolve({
+                data: articles,
+                message: 'Einträge wurden gefunden.',
+                status: 200
+            })
+        }
+        catch (err) {
+            return reject({
+                error: err,
+                status: 500,
+                message: 'Fehler beim finden vom User.'
+            })
+        }
+    });
+}
+
+const getArticleById = function (articleId) {
+    return new Promise((resolve, reject) => {
+        Article.findById(articleId)
             .then((articles) => {
                 return resolve({
                     data: articles,
@@ -50,6 +97,26 @@ const updateAllArticles = function (body) {
     });
 }
 
+function updateArticleById(body, articleId) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const article = await Article.findByIdAndUpdate(articleId, body, { new: true })
+                .catch(err => { throw err });
+            return resolve({
+                data: article,
+                message: 'Article-Update wurde durchgeführt.',
+                status: 201
+            });
+        } catch (err) {
+            return reject({
+                error: err,
+                status: 500,
+                message: 'Article-Update konnte nicht durchgeführt werden.'
+            });
+        }
+    });
+}
+
 const deleteAllArticles = function () {
     return new Promise((resolve, reject) => {
         articles = {}
@@ -72,9 +139,36 @@ const deleteAllArticles = function () {
     });
 }
 
+const deleteArticleById = function (articleId) {
+    return new Promise((resolve, reject) => {
+        Article.findById(articleId)
+            .then((articles) => {
+                copy = articles;
+                articles.delete();
+                return resolve({
+                    data: copy,
+                    message: 'Artikel wurde entfernt.',
+                    status: 200
+                })
+            })
+            .catch((err) => {
+                return reject({
+                    error: err,
+                    status: 500,
+                    message: 'Artikel konnten nicht gefunden werden.'
+                })
+            });
+    });
+}
+
+
 
 module.exports = {
     getAllArticles,
     updateAllArticles,
-    deleteAllArticles
+    deleteAllArticles,
+    getArticleById,
+    updateArticleById,
+    deleteArticleById,
+    getArticlesFromFriendsByName
 };
