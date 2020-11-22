14a07b96727cdc10aab71f3b94499a298ef5469f
s77985 <s77985@beuth-hochschule.de>
2020-11-22 14:43:50 +0100

    62::128::127:: Added Detail-Views + updated UserButton

--- Frontend/components/RootStackScreen.js
+++ Frontend/components/RootStackScreen.js
@@ -4,18 +4,19 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "./LoginScreen";
 import SplashScreen from "./SplashScreen";
-import { RegisterStackScreen, DetailViewStackScreen } from "./Headers";
+import { RegisterStackScreen, DetailEditViewStackScreen, DetailReturnViewStackScreen, ArticleRequestStackScreen } from "./Headers";
 
 const RootStack = createStackNavigator();
 
 const RootStackScreen = ({ navigation }) => (
   <RootStack.Navigator headerMode="none">
-    {/* <RootStack.Screen name="SplashScreen" component={SplashScreen} /> */}
+
+    {/* <RootStack.Screen name="ArticleRequestStackScreen" component={ArticleRequestStackScreen} />
+    <RootStack.Screen name="DetailReturnViewStackScreen" component={DetailReturnViewStackScreen} />
+    <RootStack.Screen name="DetailEditViewStackScreen" component={DetailEditViewStackScreen} />
+    <RootStack.Screen name="SplashScreen" component={SplashScreen} /> */}
     <RootStack.Screen name="LoginScreen" component={LoginScreen} />
-    <RootStack.Screen
-      name="RegisterStackScreen"
-      component={RegisterStackScreen}
-    />
+    <RootStack.Screen name="RegisterStackScreen" component={RegisterStackScreen} />
   </RootStack.Navigator>
 );
 
