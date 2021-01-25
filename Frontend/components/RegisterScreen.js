b8c997282a98dd75d6027b6880c82a6eac6d12b6
s77985 <s77985@beuth-hochschule.de>
2021-01-25 12:23:32 +0100

    fixed Register Screen

--- Frontend/components/RegisterScreen.js
+++ Frontend/components/RegisterScreen.js
@@ -158,6 +158,7 @@ const RegisterScreen = ({ navigation }) => {
             placeholder="Username"
             placeholderTextColor="#7E7E7E"
             autoCapitalize="none"
+            clearButtonMode="while-editing"
             onChangeText={(value) => usernameChange(value)}
           />
         </View>
@@ -168,6 +169,7 @@ const RegisterScreen = ({ navigation }) => {
             style={styles.inputText}
             placeholder="Passwort"
             placeholderTextColor="#7E7E7E"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handlePasswordChange(value)}
           />
         </View>
@@ -178,6 +180,7 @@ const RegisterScreen = ({ navigation }) => {
             style={styles.inputText}
             placeholder="Passwort bestätigen"
             placeholderTextColor="#7E7E7E"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handleConfirmPasswordChange(value)}
           />
         </View>
@@ -189,6 +192,7 @@ const RegisterScreen = ({ navigation }) => {
             placeholderTextColor="#7E7E7E"
             autoCapitalize="none"
             keyboardType="email-address"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handleEmailChange(value)}
           />
         </View>
@@ -200,6 +204,7 @@ const RegisterScreen = ({ navigation }) => {
             placeholderTextColor="#7E7E7E"
             autoCapitalize="none"
             keyboardType="phone-pad"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handlePhoneChange(value)}
           />
         </View>
@@ -211,6 +216,7 @@ const RegisterScreen = ({ navigation }) => {
             style={styles.inputText}
             placeholder="Straße und Hausnummer"
             placeholderTextColor="#7E7E7E"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handleStreetChange(value)}
           />
         </View>
@@ -221,6 +227,7 @@ const RegisterScreen = ({ navigation }) => {
             placeholder="PLZ"
             placeholderTextColor="#7E7E7E"
             keyboardType="phone-pad"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handlePLZChange(value)}
           />
         </View>
@@ -230,31 +237,37 @@ const RegisterScreen = ({ navigation }) => {
             style={styles.inputText}
             placeholder="Ort"
             placeholderTextColor="#7E7E7E"
+            clearButtonMode="while-editing"
             onChangeText={(value) => handleCityChange(value)}
           />
         </View>
 
-        <View style={styles.picker}>
-          <Picker
-            selectedValue={data.country}
-            style={{ backgroundColor: "#fff", width: "95%", height: 40 }}
-            itemStyle={{ justifyContent: "flex-start" }}
-            mode={"dropdown"}
-            onValueChange={(itemValue, itemIndex) =>
-              handleCountryChange(itemValue)
-            }
-          >
-            <Picker.Item label="Deutschland" value="de" />
-            <Picker.Item label="Österreich" value="au" />
-            <Picker.Item label="Schweiz" value="ch" />
-          </Picker>
+        <View style={styles.pickerView}>
+          <Text style={styles.text}>Wähle ein Land:</Text>
+
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={data.country}
+              style={styles.picker}
+              itemStyle={styles.pickerItems}
+              mode={"dropdown"}
+              onValueChange={(itemValue, itemIndex) =>
+                handleCountryChange(itemValue)
+              }
+            >
+              <Picker.Item label="Deutschland" value="de" />
+              <Picker.Item label="Österreich" value="au" />
+              <Picker.Item label="Schweiz" value="ch" />
+            </Picker>
+          </View>
         </View>
       </View>
 
       <View style={styles.userPersonalInfo}>
         <View style={styles.inputAboutMeView}>
           <TextInput
-            style={styles.inputText}
+            style={styles.inputAboutMeText}
+            multiline={true}
             placeholder="Über mich..."
             placeholderTextColor="#7E7E7E"
             onChangeText={(value) => handleAboutMEChange(value)}
@@ -263,8 +276,8 @@ const RegisterScreen = ({ navigation }) => {
       </View>
 
       <View style={styles.container}>
-        <TouchableOpacity style={styles.signUpBtn} onPress={handleRegistration}>
-          <Text style={styles.loginText}>Registrieren</Text>
+        <TouchableOpacity style={styles.button} onPress={handleRegistration}>
+          <Text style={styles.buttonText}>Registrieren</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAwareScrollView>
@@ -295,7 +308,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    zIndex: -1,
   },
   inputView: {
     width: "100%",
@@ -315,11 +327,21 @@ const styles = StyleSheet.create({
     fontSize: 14,
     height: 50,
     color: "#000",
-    marginLeft: 20,
+    marginHorizontal: 20,
   },
   inputAboutMeView: {
     width: "100%",
     height: 150,
+    paddingVertical: 10,
+    borderBottomColor: "#C6C6C8",
+    borderBottomWidth: 0.5,
+  },
+  inputAboutMeText: {
+    textAlignVertical: "top",
+    fontSize: 14,
+    height: 130,
+    color: "#000",
+    marginHorizontal: 20,
   },
   picker: {
     width: "100%",
@@ -327,7 +349,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  signUpBtn: {
+  button: {
     width: "80%",
     backgroundColor: "#E77F23",
     borderRadius: 25,
@@ -335,10 +357,58 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginTop: 20,
-    marginBottom: 20,
+    /* marginBottom: 40,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4, */
   },
-  loginText: {
+  buttonText: {
     color: "#fff",
     fontSize: 20,
+  },
+  pickerView: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    alignItems: "center",
+    borderBottomColor: "#C6C6C8",
+    borderBottomWidth: 0.5,
+    ...Platform.select({
+      ios: {
+        height: 88,
+        paddingHorizontal: 20,
+      },
+      android: {
+        height: 50,
+        paddingHorizontal: 15,
+      },
+      default: {
+        height: 50,
+        paddingHorizontal: 15,
+      },
+    }),
+  },
+  pickerContainer: {
+    //flex: 1,
+    width: "65%",
+    height: "100%",
+    justifyContent: "center",
+  },
+  picker: {
+    width: "100%",
+    height: 88,
+    fontSize: 14,
+    color: "#000",
+  },
+  pickerItems: {
+    fontSize: 14,
+    height: 88,
+    color: "#000",
+  },
+  text: {
+    width: "35%",
+    fontSize: 14,
+    color: "#7E7E7E",
+    alignContent: "center",
   },
 });
