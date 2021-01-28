dc2e83272663a8c744471a22082f73532f800e59
John <einickejohn@gmail.com>
2021-01-28 16:55:27 +0100

    remove verify route

--- Backend/app.js
+++ Backend/app.js
@@ -24,13 +24,6 @@ app.use('/articles', allRoutes.articlesRoute);
 app.use('/auth', allRoutes.authRoute);
 app.use("/articleRequest", allRoutes.articleRequestRoute);
 
-app.get("/.well-known/pki-validation/F72E950EE01A5C306B6B90AF3BB71E16.txt", (req, res) => {
-  var text = '953BBDECA5A01578BC3CAC988670E3ED30FE76FF5729AC110EB550DE8A8B1608\ncomodoca.com\n7a82026923f2108'
-  res.attachment('F72E950EE01A5C306B6B90AF3BB71E16.txt')
-  res.type('txt')
-  res.send(text)
-});
-
 // Error Handling
 app.use((err, req, res, next) => {
   handleError(err, res);
